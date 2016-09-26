170867c97ff5a1bab1da85866a492836857e9d6a
eonjeong <awsed33@naver.com>
2016-09-26 15:07:24 +0900

    first

---
+++ treepod/deploy_result.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+
+</head>
+<body>
+<?php
+include('api_constants.php');
+include ('./callAPI.php');
+include('var_dump_enter.php');
+var_dump_enter($_POST);
+
+$URL = "https://api.ucloudbiz.olleh.com/server/v1/client/api?";
+
+$listProductcmdArr = array(
+    "command" => "deployVirtualMachine",
+    "serviceofferingid" => $_POST['serviceofferingid'],
+    "templateid" => $_POST['templateid'],
+    "diskofferingid" => $_POST['diskofferingid'], 
+//    "productcode" => $_POST['productid'],
+    "zoneid" => $_POST['zoneid'],
+    "displayname" => "jjkserver2",
+    "usageplantype" => "hourly",
+    "apikey" => API_KEY
+);
+var_dump_enter($listProductcmdArr);
+$result = callCommand($URL, $listProductcmdArr, SECERET_KEY);
+sleep(1);
+$jobId = $result["jobid"];
+echo $jobId;
+/*
+do {
+  $cmdArr2 = array(
+    "command" => "queryAsyncJobResult",
+    "jobid" => $jobId,
+    "apikey"  => API_KEY
+  );
+  $result2 = callCommand($URL, $cmdArr2, SECERET_KEY);
+  sleep(5);
+  $jobStatus = $result2["jobstatus"];
+  if ($jobStatus == 2) {
+     printf($result2["jobresult"]);
+      exit;
+  }
+} while ($jobStatus != 1);
+*/
+?>
+</body>
+</html>
